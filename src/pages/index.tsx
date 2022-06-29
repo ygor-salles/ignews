a2fd398269283bdc664bca5a99a67ef2742445df
ygor-salles <ygorsallesaniceto@gmail.com>
2022-06-28 23:01:39 -0300

    installing sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,9 @@
+import styles from "../styles/home.module.scss";
+
 export default function Home() {
-  return <h1>hellow world</h1>;
+  return (
+    <h1 className={styles.title}>
+      hellow <span>world</span>
+    </h1>
+  );
 }
