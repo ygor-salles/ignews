dd3230c040a793f02dcbf8c7a3464c61d39284d1
ygor-salles <ygorsallesaniceto@gmail.com>
2022-06-28 23:38:43 -0300

    global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
-import styles from "../styles/home.module.scss";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      hellow <span>world</span>
-    </h1>
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1>Hellow World</h1>
+    </>
   );
 }
