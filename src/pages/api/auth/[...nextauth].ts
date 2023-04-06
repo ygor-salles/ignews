e0940db9ddb3dad099eabf75996c4fd72c14b7dd
ygor-salles <ygorsallesaniceto@gmail.com>
2023-04-05 23:14:07 -0300

    feat:add database faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,33 @@
+import { query as q } from "faunadb";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
-const authOptions = {
+import { fauna } from "../../../services/fauna";
+
+export default NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
-};
-export default NextAuth(authOptions);
+  session: {
+    strategy: "jwt",
+  },
+  jwt: {
+    secret: process.env.SIGNING_KEY,
+  },
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      try {
+        await fauna.query(
+          q.Create(q.Collection("users"), { data: { email: user.email } })
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
+});
