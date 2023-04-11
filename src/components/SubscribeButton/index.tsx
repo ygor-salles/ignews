baea85a08fc7953c25792a9de121b998950d000d
ygor-salles <ygorsallesaniceto@gmail.com>
2023-04-10 21:58:04 -0300

    feat: rule Stripe session

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,7 @@
 import { useSession, signIn } from "next-auth/react";
 import styles from "./styles.module.scss";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 
 interface SubscribeButtonProps {
   priceId: string;
@@ -8,10 +10,22 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const { data: session, status } = useSession();
 
-  const handleSubscribe = () => {
+  const handleSubscribe = async () => {
     if (!session) {
       signIn("github");
       return;
+    }
+
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert(error.message);
     }
   };
 
